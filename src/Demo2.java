4daee26b56448a06f13463e22da134e81e39cf9c
zhuochujie <2662648243@qq.com>
2019-01-04 14:08:15 +0800

    抽象类

--- src/Demo2.java
+++ src/Demo2.java
@@ -1,17 +1,26 @@
+//abstract class A{
+//    public void fun(){
+//        System.out.println("存在有方法体的方法");
+//    }
+//    public abstract void print();//此方法没有方法体,并且存在abstract关键字,表示抽象方法
+//}
+//class B extends A{
+//    public void print(){
+//        System.out.println("hello world");
+//    }
+//}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 abstract class A{
-    public void fun(){
-        System.out.println("存在有方法体的方法");
-    }
-    public abstract void print();//此方法没有方法体,并且存在abstract关键字,表示抽象方法
-}
-class B extends A{
     public void print(){
-        System.out.println("hello world");
+        System.out.println("抽象类print方法");
     }
+}
+
+class X extends A{
 }
 public class Demo2 {
     public static void main(String[] args) {
-        A a = new B();
-        a.print();
+        X x = new X();
+        x.print();
     }
 }
